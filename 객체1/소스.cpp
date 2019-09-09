37c2ce2c8613435e5b34509a13df5250cf6c681a
dlqjatn9862@naver.com <dlqjatn9862@naver.com>
2019-09-09 10:30:17 +0900

    aaaaaa

---
+++ 객체1/소스.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+class LetDebug
+{
+public:
+	void printNum() {
+		/*
+		short s1 = 32767;
+	    short s2 = 1;
+	    short s3 = s1 + s2;
+	    std::cout << s3 << std::endl;
+		*/
+		nsinged short s1 = 32767;
+		short s2 = 1;
+		short s3 = s1 + s2;
+		std::cout << s3 << std::endl;
+	}
+};
+int main() {
+	LetDebug* ld = new LetDebug;
+	ld->printNum();
+
+	system("pause");
+	return 0;
+}
