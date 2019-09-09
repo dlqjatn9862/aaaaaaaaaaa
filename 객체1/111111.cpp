cbc407339c1c61916596b301e12d006194755d8d
dlqjatn9862@naver.com <dlqjatn9862@naver.com>
2019-09-09 10:34:43 +0900

    aa

--- 객체1/111111.cpp
+++ 객체1/111111.cpp
@@ -5,20 +5,16 @@ public:
 	void printNum() {
 		/*
 		short s1 = 32767;
-	    short s2 = 1;
-	    short s3 = s1 + s2;
-	    std::cout << s3 << std::endl;
-		*/
-		unsinged short s1 = 32767;
 		short s2 = 1;
 		short s3 = s1 + s2;
 		std::cout << s3 << std::endl;
+		*/
 	}
 };
 int main() {
 	LetDebug* ld = new LetDebug;
 	ld->printNum();
-	std:: cout << "201602039" << std:: endl;
+
 	system("pause");
 	return 0;
-} 
+}
